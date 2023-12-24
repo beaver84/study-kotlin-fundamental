13bb1021bce631c38f8dd20603de3decaca1c6d5
bae kyungtae <b841010@naver.com>
2023-12-24 17:30:57 +0900

    코틀린에서 배열과 컬렉션을 다루는 방법

--- src/main/java/com/lannstark/lec15/Lec15Main.kt
+++ src/main/java/com/lannstark/lec15/Lec15Main.kt
@@ -2,11 +2,43 @@ package com.lannstark.lec15
 
 fun main() {
 
-    val oldMap = mutableMapOf<Int, String>()
-    oldMap[1] = "MONDAY"
-    oldMap[2] = "MONDAY"
+    val array = arrayOf(100, 200)
 
-    mapOf(1 to "MONDAY", 2  to "TUESDAY")
+//    for (i in array.indices) {
+//        println("${i} ${array[i]}")
+//    }
+
+//    for ((idx,value) in array.withIndex()) {
+//        println("$idx $value")
+//    }
+
+//    val numbers = listOf(100, 200)
+//
+//    println(numbers[0])
+//
+//    for (number in numbers) {
+//        println(number)
+//    }
+//
+//    for ((idx, value) in numbers.withIndex()) {
+//        println("${idx} ${value}")
+//    }
+
+//   val numbers = setOf(100, 100, 200)
+//
+//    for (number in numbers) {
+//        println(number)
+//    }
+//
+//    for ((index, number) in numbers.withIndex()) {
+//        println("$index $number")
+//    }
+
+//    val oldMap = mutableMapOf<Int, String>()
+//    oldMap[1] = "MONDAY"
+//    oldMap[2] = "MONDAY"
+
+    val oldMap = mapOf(1 to "MONDAY", 2 to "TUESDAY")
 
     for (key in oldMap.keys) {
         print(key)
@@ -18,3 +50,4 @@ fun main() {
         println(value)
     }
 }
+
